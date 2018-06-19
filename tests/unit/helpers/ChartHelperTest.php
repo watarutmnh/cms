60c7f212d3655801001b65bb1cde81d029bdb3f8
Makeila Lundy <makeila@pixelandtonic.com>
2018-06-19 09:42:28 -0700

    added chart helper test class

---
+++ tests/unit/helpers/ChartHelperTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\helpers;
+
+use yii\db\Query;
+
+
+
+class ChartHelperTest extends \Codeception\TestCase\Test
+{
+
+public $query = $this->make(new Query());
+public $datetime = $this->make(new \DateTime());
+
+
+
+}
